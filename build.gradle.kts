7c31a9fb0aa652c7e7ca19f6ebbe9a2b41884de8
Michael Stücklschweiger <michael.stuecklschweiger@gmail.com>
2024-03-19 20:12:50 +0100

    sonarcloud setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,14 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.2.0" apply false
+    id("org.sonarqube") version "4.4.1.3373"
+
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "SE2-2024-Group-Alpha_RiskApp")
+        property("sonar.organization", "se2-2024-group-alpha")
+        property("sonar.host.url", "https://sonarcloud.io/")
+    }
 }
