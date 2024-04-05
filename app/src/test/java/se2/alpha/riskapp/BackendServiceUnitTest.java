06e67020d4c261877f45a39257594a48f7ce93b0
Simon Schadenbauer <simonsc@edu.aau.at>
2024-04-05 22:05:10 +0200

    backend service unit tests done

--- app/src/test/java/se2/alpha/riskapp/BackendServiceUnitTest.java
+++ app/src/test/java/se2/alpha/riskapp/BackendServiceUnitTest.java
@@ -11,23 +11,20 @@ import org.json.JSONException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import javax.inject.Inject;
-
 import org.mockito.*;
-import se2.alpha.riskapp.model.auth.JwtAuthenticationResponse;
+
 import se2.alpha.riskapp.model.auth.SignInRequest;
+import se2.alpha.riskapp.model.auth.SignUpRequest;
+import se2.alpha.riskapp.model.auth.ValidationRequest;
+import se2.alpha.riskapp.model.game.CreateLobbyRequest;
 import se2.alpha.riskapp.service.BackendService;
 import se2.alpha.riskapp.service.GameService;
-import se2.alpha.riskapp.service.LobbyService;
 import se2.alpha.riskapp.service.SecurePreferencesService;
-
-import java.io.IOException;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
 
 public class BackendServiceUnitTest {
 
-    private BackendService backendServiceSpy;
+    private BackendService backendServiceSuccessSpy;
+    private BackendService backendServiceErrorSpy;
 
     @Captor
     ArgumentCaptor<Request> requestCaptor;
@@ -50,24 +47,116 @@ public class BackendServiceUnitTest {
 
         BackendService backendService = new BackendService(context, securePreferencesService, gameService);
 
-        backendServiceSpy = Mockito.spy(backendService);
+        backendServiceSuccessSpy = Mockito.spy(backendService);
         doAnswer(invocation -> {
             Request capturedRequest = requestCaptor.getValue();
             BackendService.NetworkCallback capturedCallback = callbackCaptor.getValue();
             if(capturedRequest.url().toString().endsWith("signin")) {
                 capturedCallback.onResult("{ \"token\": \"tokenTest\"}");
+            } else if(capturedRequest.url().toString().endsWith("signup")) {
+                capturedCallback.onResult("{ \"token\": \"tokenTest\"}");
+            } else if(capturedRequest.url().toString().endsWith("validate")) {
+                capturedCallback.onResult("true");
+            } else if(capturedRequest.url().toString().endsWith("lobby")) {
+                capturedCallback.onResult("{\"gameSessionId\": \"7d7ef1d5-89d2-4c8a-8166-7c273b6b5202\"}");
             }
             return null;
-        }).when(backendServiceSpy).executeRequest(requestCaptor.capture(), callbackCaptor.capture(), any());
-    }
+        }).when(backendServiceSuccessSpy).executeRequest(requestCaptor.capture(), callbackCaptor.capture(), any());
+
+    backendServiceErrorSpy = Mockito.spy(backendService);
+    doAnswer(invocation -> {
+        Request capturedRequest = requestCaptor.getValue();
+        BackendService.NetworkCallback capturedCallback = callbackCaptor.getValue();
+        if(capturedRequest.url().toString().endsWith("signin")) {
+            capturedCallback.onResult("{ }");
+        } else if(capturedRequest.url().toString().endsWith("signup")) {
+            capturedCallback.onResult("{ }");
+        } else if(capturedRequest.url().toString().endsWith("validate")) {
+            capturedCallback.onResult("{ }");
+        } else if(capturedRequest.url().toString().endsWith("lobby")) {
+            capturedCallback.onResult("{}");
+        }
+        return null;
+    }).when(backendServiceErrorSpy).executeRequest(requestCaptor.capture(), any(), callbackCaptor.capture());
+}
 
     @Test
-    void testMakeSignInRequest() throws JSONException {
+    void testMakeSignInRequestSuccess() throws JSONException {
         SignInRequest signInRequest = new SignInRequest("test", "test");
         BackendService.SignInCallback signInCallback = mock(BackendService.SignInCallback.class);
 
-        backendServiceSpy.makeSignInRequest(signInRequest, signInCallback);
+        backendServiceSuccessSpy.makeSignInRequest(signInRequest, signInCallback);
 
         verify(signInCallback, Mockito.timeout(1000)).onSuccess(any());
+    }
+
+    @Test
+    void testMakeSignInRequestError() throws JSONException {
+        SignInRequest signInRequest = new SignInRequest("test", "test");
+        BackendService.SignInCallback signInCallback = mock(BackendService.SignInCallback.class);
+
+        backendServiceErrorSpy.makeSignInRequest(signInRequest, signInCallback);
+
+        verify(signInCallback, Mockito.timeout(1000)).onError(any());
+    }
+
+    @Test
+    void testMakeSignUpRequestSuccess() throws JSONException {
+        SignUpRequest signUpRequest = new SignUpRequest("test", "test");
+        BackendService.SignUpCallback signUpCallback = mock(BackendService.SignUpCallback.class);
+
+        backendServiceSuccessSpy.makeSignUpRequest(signUpRequest, signUpCallback);
+
+        verify(signUpCallback, Mockito.timeout(1000)).onSuccess(any());
+    }
+
+    @Test
+    void testMakeSignUpRequestError() throws JSONException {
+        SignUpRequest signUpRequest = new SignUpRequest("test", "test");
+        BackendService.SignUpCallback signUpCallback = mock(BackendService.SignUpCallback.class);
+
+        backendServiceErrorSpy.makeSignUpRequest(signUpRequest, signUpCallback);
+
+        verify(signUpCallback, Mockito.timeout(1000)).onError(any());
+    }
+
+    @Test
+    void testMakeValidationRequestSuccess() throws JSONException {
+        ValidationRequest validationRequest = new ValidationRequest("");
+        BackendService.ValidationCallback validationCallback = mock(BackendService.ValidationCallback.class);
+
+        backendServiceSuccessSpy.makeValidationRequest(validationRequest, validationCallback);
+
+        verify(validationCallback, Mockito.timeout(1000)).onSuccess(true);
+    }
+
+    @Test
+    void testMakeValidationRequestError() throws JSONException {
+        ValidationRequest validationRequest = new ValidationRequest("");
+        BackendService.ValidationCallback validationCallback = mock(BackendService.ValidationCallback.class);
+
+        backendServiceErrorSpy.makeValidationRequest(validationRequest, validationCallback);
+
+        verify(validationCallback, Mockito.timeout(1000)).onError(any());
+    }
+
+    @Test
+    void testCreateLobbyRequestSuccess() throws JSONException {
+        CreateLobbyRequest createLobbyRequest = new CreateLobbyRequest();
+        BackendService.CreateLobbyCallback createLobbyCallback = mock(BackendService.CreateLobbyCallback.class);
+
+        backendServiceSuccessSpy.createLobbyRequest(createLobbyRequest, createLobbyCallback);
+
+        verify(createLobbyCallback, Mockito.timeout(1000)).onSuccess(any());
+    }
+
+    @Test
+    void testCreateLobbyRequestError() throws JSONException {
+        CreateLobbyRequest createLobbyRequest = new CreateLobbyRequest();
+        BackendService.CreateLobbyCallback createLobbyCallback = mock(BackendService.CreateLobbyCallback.class);
+
+        backendServiceErrorSpy.createLobbyRequest(createLobbyRequest, createLobbyCallback);
+
+        verify(createLobbyCallback, Mockito.timeout(1000)).onError(any());
     }
 }
