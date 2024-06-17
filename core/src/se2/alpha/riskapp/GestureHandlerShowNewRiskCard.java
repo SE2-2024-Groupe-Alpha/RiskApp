67f26f7c5f5a1919a05f88cbbf139f0ccec345dd
Almin <alminkadiric7@gmail.com>
2024-06-17 08:27:06 +0200

    Encapsulated fields by marking overlayShowRiskCards, buttonLeft, and buttonRight as private and final.

--- core/src/se2/alpha/riskapp/GestureHandlerShowNewRiskCard.java
+++ core/src/se2/alpha/riskapp/GestureHandlerShowNewRiskCard.java
@@ -7,12 +7,22 @@ import se2.alpha.riskapp.ui.OverlayShowAllRiskCards;
 import se2.alpha.riskapp.ui.OverlayShowNewRiskCard;
 
 public class GestureHandlerShowNewRiskCard extends GestureDetector.GestureAdapter {
-    OverlayShowNewRiskCard overlayShowNewRiskCard;
+   private  final OverlayShowNewRiskCard overlayShowNewRiskCard;
 
     public GestureHandlerShowNewRiskCard(OverlayShowNewRiskCard overlayShowNewRiskCard)
     {
         this.overlayShowNewRiskCard = overlayShowNewRiskCard;
     }
+
+    /**
+     * Handles tap events. Hides the overlay when tapped.
+     *
+     * @param x the x coordinate of the tap
+     * @param y the y coordinate of the tap
+     * @param count the number of taps
+     * @param button the button pressed
+     * @return true to indicate the event was handled
+     */
     @Override
     public boolean tap(float x, float y, int count, int button) {
         overlayShowNewRiskCard.hide();
